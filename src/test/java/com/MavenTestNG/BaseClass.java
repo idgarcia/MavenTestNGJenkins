c7ce760c2fc2ac8f6bde1d9678999f8dc41ffc64
idgarcia71090 <idgarcia71090@gmail.com>
2018-12-29 12:21:01 -0500

    new Changes

--- src/test/java/com/MavenTestNG/BaseClass.java
+++ src/test/java/com/MavenTestNG/BaseClass.java
@@ -4,18 +4,24 @@ package com.MavenTestNG;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class BaseClass {
+	
 	@Test
 	public void setUp() {
 		System.setProperty("webdriver.chrome.driver", "com.MavenTestNGJenkins/Drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
+		String Title = driver.getTitle();
+		Assert.assertEquals(Title, "Google");
+	}
+
 
 	
 
 		
 	}
 		
-}
+
